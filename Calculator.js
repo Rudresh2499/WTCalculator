a72b7eae1677fa1c22d4e04d327820ed751741bf
Rudresh2499 <61000149+Rudresh2499@users.noreply.github.com>
2020-03-04 00:06:52 +0530

    Added Error Handling

--- Calculator.js
+++ Calculator.js
@@ -16,10 +16,39 @@ function getInputField()
 
 function solution()
 {
-	let y = getInputField()
-	let sol = eval(y)
+	let expression = getInputField()
+	var sol
+	try
+	{
+		sol = eval(expression)
+	}
+	catch(error)
+	{
+		switch(error.name)
+		{
+			case 'SyntaxError': while(expression.charAt(expression.length-1)==='+'||expression.charAt(expression.length-1)==='-'||expression.charAt(expression.length-1)==='*'||expression.charAt(expression.length-1)==='/')
+								{
+									let newExpression=expression.substring(0,expression.length-1)
+									expression = newExpression
+								}
+								sol=eval(expression)
+								break;
+			
+			case 'ReferenceError': 	sol='Invalid Reference used'
+									break;
+
+			case 'RangeError': 	sol='Solution out of range'
+								break;
+
+			case 'EvalError': 	sol='Internal Error'
+								break;
+
+			default : 	sol='Some Error Occured'
+						break;
+		}
+	}
 	clr()
-	document.getElementById('inputField').value = document.getElementById('inputField').value+sol
+	document.getElementById('inputField').value=document.getElementById('inputField').value+sol
 }
 
 function changeToDarkMode()
